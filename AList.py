87796e029c64b6782900ba2d8436fd18ef41d376
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-07-08 14:34:44 +0200

    Create beginning of AList

--- AList.py
+++ AList.py
@@ -1,95 +1,28 @@
+from tkinter import ttk
 from tkinter import *
 from jikanpy import Jikan
-import os
 
-from files.python.menu import Menu
-from files.python.accueil import Accueil
-from files.python.ranime import rAnime
-from files.python.rmanga import rManga
-from files.python.lanime import lAnime
-from files.python.lmanga import lManga
-from files.python.manga import Manga
-from files.python.anime import Anime
-from files.python.manime import mAnime
-from files.python.mmanga import mManga
-from files.python.tops import Tops
+from alist.utils import Config
+
 
 class Main(Tk):
     def __init__(self):
-        """Fenêtre principale du logiciel"""
         super(Main, self).__init__()
-        self.pageActuel = ""
         self.jikan = Jikan()
+        self.config = Config()
+
+        self.tk.call('source', 'alist/themes/' + self.config.get("theme", "azure-dark") + '.tcl')
+        ttk.Style().theme_use(self.config.get("theme", "azure-dark"))
         
         self.title("AList")
+        self.geometry("1280x900")
         self.resizable(width=False, height=False)
 
-        self.titre = Label(self, text = "AList", font="-size 25 -weight bold")
+        self.titre = ttk.Label(self, text="AList")
         self.titre.pack()
 
-        self.menu = Menu(self)
-
-        self.page = Frame(self)
-        self.page.pack(side=RIGHT)
-        
-        self.showPage("accueil")
-
         self.mainloop()
-    
-    def showPage(self, page):
-        """Affiche la page <page>
-        Certaines pages ont un code pour avoir un autre argument comme "anime|<nom>"
-        """
-        if self.pageActuel != page:
-            self.page.destroy()
-            if page == "accueil" or (page == "reload" and self.pageActuel == "accueil"):
-                self.page = Accueil(self, self.jikan)
-            elif page == "tops" or (page == "reload" and self.pageActuel == "tops"):
-                self.page = Tops(self, self.jikan)
-            elif page == "rAnime" or (page == "reload" and self.pageActuel == "rAnime"):
-                self.page = rAnime(self, self.jikan)
-            elif page == "rManga" or (page == "reload" and self.pageActuel == "rManga"):
-                self.page = rManga(self, self.jikan)
-            elif page == "lAnime" or (page == "reload" and self.pageActuel == "lAnime"):
-                self.page = lAnime(self, self.jikan)
-            elif page == "lManga" or (page == "reload" and self.pageActuel == "lManga"):
-                self.page = lManga(self, self.jikan)
-            elif page == "reload" and len(self.pageActuel.split("|")) == 2:
-                if self.pageActuel.split("|")[0] == "anime":
-                    self.page = Anime(self, self.jikan, self.pageActuel.split("|")[1])
-                elif self.pageActuel.split("|")[0] == "manga":
-                    self.page = Manga(self, self.jikan, self.pageActuel.split("|")[1])
-                elif self.pageActuel.split("|")[0] == "animeM":
-                    self.page = mAnime(self, self.jikan, self.pageActuel.split("|")[1])
-                elif self.pageActuel.split("|")[0] == "mangaM":
-                    self.page = mManga(self, self.jikan, self.pageActuel.split("|")[1])
-                else:
-                    self.page = Frame(self)
-                    self.page.pack_propagate(False)
-                    self.page.config(width=800, height=600)
-                    self.page.pack(side=RIGHT)
-            else:
-                if len(page.split("|")) == 2:
-                    if page.split("|")[0] == "anime":
-                        self.page = Anime(self, self.jikan, page.split("|")[1])
-                    elif page.split("|")[0] == "manga":
-                        self.page = Manga(self, self.jikan, page.split("|")[1])
-                    elif page.split("|")[0] == "animeM":
-                        self.page = mAnime(self, self.jikan, page.split("|")[1])
-                    elif page.split("|")[0] == "mangaM":
-                        self.page = mManga(self, self.jikan, page.split("|")[1])
-                    else:
-                        self.page = Frame(self)
-                        self.page.pack_propagate(False)
-                        self.page.config(width=800, height=600)
-                        self.page.pack(side=RIGHT)
-                else:
-                    self.page = Frame(self)
-                    self.page.pack_propagate(False)
-                    self.page.config(width=800, height=600)
-                    self.page.pack(side=RIGHT)
-            self.pageActuel = page
 
-os.makedirs("files/anime", exist_ok=True)
-os.makedirs("files/manga", exist_ok=True)
-Main()
+
+if __name__ == "__main__":
+    Main()
