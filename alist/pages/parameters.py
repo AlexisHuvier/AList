dcf6b9bb8d42820a98d6fd80939ef34e9dae9653
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-07-09 18:04:01 +0200

    Create Parameters

--- alist/pages/parameters.py
+++ alist/pages/parameters.py
@@ -9,4 +9,31 @@ class Parameters(RightPage):
         title = ttk.Label(self, text="Paramètres", font="-size 22 -weight bold")
         title.pack(pady=15)
 
+        theme_label = ttk.Label(self, text="Thème :", font="-size 14")
+        theme_label.pack(pady=(30, 0))
+
+        self.theme = StringVar(self)
+        self.theme.set(self.main.config.get("theme", "azure-dark"))
+        themes = [self.main.config.get("theme", "azure-dark")]
+        themes += [i.split(".")[0] for i in os.listdir("alist/themes") if ".tcl" in i and i.split(".")[0] not in themes]
+        theme_choose = ttk.OptionMenu(self, self.theme, *themes)
+        theme_choose["width"] = 20
+        theme_choose.pack(pady=15)
+
+        self.translation = IntVar(self)
+        self.translation.set(1 if self.main.config.get("translation", True) else 0)
+        translation_checkbox = ttk.Checkbutton(self, text="Traduction Automatique", variable=self.translation,
+                                               onvalue=1, offvalue=0)
+        translation_checkbox.pack(pady=(30, 15))
+
+        valid = ttk.Button(self, width=20, text="Valider", command=self.validate)
+        valid.pack(pady=30)
+
         self.pack(side=RIGHT, fill=BOTH)
+
+    def validate(self):
+        self.main.config.set("theme", self.theme.get())
+        self.main.config.set("translation", True if self.translation.get() == 1 else False)
+        self.main.config.save()
+        messagebox.showwarning("AList - Paramètres", "Certains paramètres nécessitent de relancer le programme.")
+        self.main.show_page("accueil")
