cad1bb5d297cda6b684e9646432b59f65e5537f4
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-07-11 01:03:45 +0200

    Little changes in RightPage (PEP8 + grid_propagate)

--- alist/pages/right_page.py
+++ alist/pages/right_page.py
@@ -1,11 +1,12 @@
 from tkinter import ttk
-from tkinter import *
+from tkinter import GROOVE
 
 
 class RightPage(ttk.Frame):
     def __init__(self, main):
         super(RightPage, self).__init__(main, relief=GROOVE)
         self.pack_propagate(False)
+        self.grid_propagate(False)
         self["padding"] = 30
         self.config(width=980, height=800)
         self.main = main
