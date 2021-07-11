5b20b65dc2c3e787cdc984763ffa368cf12e41e7
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-07-11 23:09:03 +0200

    Finish ModifAnime and ModifManga Pages

--- alist/pages/modif_anime.py
+++ alist/pages/modif_anime.py
@@ -1,4 +1,4 @@
-from tkinter import ttk, RIGHT, BOTH
+from tkinter import ttk, RIGHT, BOTH, StringVar
 
 from alist.pages.right_page import RightPage
 
@@ -8,7 +8,29 @@ class ModifAnime(RightPage):
         super(ModifAnime, self).__init__(main)
         self.anime = self.main.myanime.get(mal_id)
 
-        title = ttk.Label(self, text="MODIF ANIME", font="-size 22 -weight bold")
-        title.pack(pady=15)
+        title = ttk.Label(self, text=self.anime["name"], font="-size 22 -weight bold")
+        title.pack(pady=20)
+
+        status = ttk.Label(self, text="Statut :")
+        status.pack(pady=(15, 10))
+        self.status = StringVar(self)
+        self.status.set(self.anime["status"])
+        temp = ["A voir", "En visionnement", "Fini", "Abandonné"]
+        temp.remove(self.anime["status"])
+        stat = ttk.OptionMenu(self, self.status, self.anime["status"], self.anime["status"], *temp)
+        stat["width"] = 20
+        stat.pack(pady=(0, 15))
+
+        ep = ttk.Label(self, text="Nombre d'épisodes vus (/"+str(self.anime["max_ep"])+"):")
+        ep.pack(pady=(15, 0))
+        self.ep = ttk.Spinbox(self, from_=0., to=self.anime["max_ep"], width=20)
+        self.ep.set(self.anime["ep"])
+        self.ep.pack(pady=15)
+
+        valid = ttk.Button(self, text="Valider", width=20, command=self.validate)
+        valid.pack(pady=20)
 
         self.pack(side=RIGHT, fill=BOTH)
+
+    def validate(self):
+        self.main.myanime.modify(self.anime["id"], status=self.status.get(), ep=int(self.ep.get()))
--- alist/pages/modif_manga.py
+++ alist/pages/modif_manga.py
@@ -1,4 +1,4 @@
-from tkinter import ttk, RIGHT, BOTH
+from tkinter import ttk, RIGHT, BOTH, StringVar
 
 from alist.pages.right_page import RightPage
 
@@ -8,7 +8,36 @@ class ModifManga(RightPage):
         super(ModifManga, self).__init__(main)
         self.manga = self.main.mymanga.get(mal_id)
 
-        title = ttk.Label(self, text="MODIF MANGA", font="-size 22 -weight bold")
-        title.pack(pady=15)
+        title = ttk.Label(self, text=self.manga["name"], font="-size 22 -weight bold")
+        title.pack(pady=20)
+
+        status = ttk.Label(self, text="Statut :")
+        status.pack(pady=(15, 10))
+        self.status = StringVar(self)
+        self.status.set(self.manga["status"])
+        temp = ["A voir", "En visionnement", "Fini", "Abandonné"]
+        temp.remove(self.manga["status"])
+        stat = ttk.OptionMenu(self, self.status, self.manga["status"], self.manga["status"], *temp)
+        stat["width"] = 20
+        stat.pack(pady=(0, 15))
+
+        vol = ttk.Label(self, text="Nombre de volumes vus (/"+str(self.manga["max_vol"])+"):")
+        vol.pack(pady=(15, 0))
+        self.vol = ttk.Spinbox(self, from_=0., to=self.manga["max_vol"], width=20)
+        self.vol.set(self.manga['vol'])
+        self.vol.pack(pady=15)
+
+        chap = ttk.Label(self, text="Nombre de chapitres vus (/"+str(self.manga["max_chap"])+"):")
+        chap.pack(pady=(15, 0))
+        self.chap = ttk.Spinbox(self, from_=0., to=self.manga["max_chap"], width=20)
+        self.chap.set(self.manga['chap'])
+        self.chap.pack(pady=15)
+
+        valid = ttk.Button(self, text="Valider", width=20, command=self.validate)
+        valid.pack(pady=20)
 
         self.pack(side=RIGHT, fill=BOTH)
+
+    def validate(self):
+        self.main.mymanga.modify(self.manga["id"], status=self.status.get(), vol=int(self.vol.get()),
+                                chap=int(self.chap.get()))
