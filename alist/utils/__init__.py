9ee22277a9997fdf6175d7875243cfa58135a037
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-07-14 20:36:17 +0200

    Create pretty_number function

--- alist/utils/__init__.py
+++ alist/utils/__init__.py
@@ -1,3 +1,4 @@
 from alist.utils.config import Config
 from alist.utils.scroll_frame import ScrollFrame
 from alist.utils.google_translate import GoogleTranslator
+from alist.utils.utils import *
--- alist/utils/utils.py
+++ alist/utils/utils.py
@@ -3,3 +3,8 @@ import webbrowser
 
 def open_url(url):
     webbrowser.open(url)
+
+
+def pretty_number(nb):
+    nb = str(nb)
+    return " ".join((nb[::-1][i: i + 3] for i in range(0, len(nb), 3)))[::-1]
