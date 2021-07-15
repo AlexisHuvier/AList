f411adb608ac0f677d580827ce22a19a8cb10310
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-07-15 14:03:30 +0200

    Change padding in RightPage

--- alist/pages/right_page.py
+++ alist/pages/right_page.py
@@ -7,6 +7,6 @@ class RightPage(ttk.Frame):
         super(RightPage, self).__init__(main, relief=GROOVE)
         self.pack_propagate(False)
         self.grid_propagate(False)
-        self["padding"] = 5
+        self["padding"] = 10
         self.config(width=980, height=800)
         self.main = main
